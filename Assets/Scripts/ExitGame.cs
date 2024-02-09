d72a5b4a2f662e2bfce48e55a15e2d398a3eb2ca
Robin <97387960+robeecodes@users.noreply.github.com>
2024-02-09 11:36:56 +0000

    Create ExitGame

--- Assets/Scripts/ExitGame.cs
+++ Assets/Scripts/ExitGame.cs
@@ -1,8 +1,13 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ExitGame : MonoBehaviour {
-    
-    
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            Debug.Log("Goodbye!");
+            Application.Quit();
+        }
+    }
 }
